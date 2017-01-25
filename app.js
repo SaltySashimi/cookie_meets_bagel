72ed3fc94f98efb2f55ff076cca457f4a51990ee
Michael Li <lvlichaelly@gmail.com>
2017-01-25 00:17:35 -0800

    [feature] established models and sequelize setup

--- app.js
+++ app.js
@@ -1,45 +1,35 @@
 const express = require('express');
-const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan'); // logging framework
+const router = require('./router');
 const webpack = require('webpack');
 const config = require('./webpack.config.dev');
-const router = require('./router');
 
 // var favicon = require('serve-favicon');
 
 const app = express();
 
+app.set('models', require('./server/models/'));
 
-// const sequelize = require('./db/sequelize.js');
-// const User = require('./models/user.js');
-// const mongoose = require('mongoose');
-
-// DB Setup
-app.set('models', require('./models')); // var User = app.get('models').User;
-// mongoose.connect('mongodb://localhost:auth/auth');
+// setTimeout(function () {
+//   app.get('models').User.create({
+//       username: 'coolmike',
+//       email: 'mike5@example.om'
+//     }).then(function(user) {
+//       console.log(user.Instance)
+//     })
+// }, 5000)
 
 // App Setup
 app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json({ type: '*/*' })); // any incoming request will be parsed as json
+
+// Router
 router(app);
 
-// Server Setup
-const port = process.env.PORT || 3000;
-const server = http.createServer(app);
-app.listen(port, function onAppListening(err) {
-  if (err) {
-    console.error(err);
-  } else {
-    console.info('==> 🚧  Webpack development server listening on port %s', port);
-  }
-});
-
-// Webpack Compiler
 const compiler = webpack(config);
 app.use(require('webpack-dev-middleware')(compiler, {noInfo: true, publicPath: config.output.publicPath}));
 app.use(require('webpack-hot-middleware')(compiler));
-
 
 module.exports = app;
--- router.js
+++ router.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = function(app) {
   app.get('/', function(req, res, next) {
-    // res.sendFile(path.join(__dirname, 'index.html'));
-    res.send(['cat', 'dog'])
+    res.sendFile(path.join(__dirname, 'index.html'));
+    // res.send(['cat', 'dog', 'dinosaur', 'fish'])
   });
 }
